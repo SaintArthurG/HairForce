c437ef688f4c79706f13594447459ff05a170cad
SaintArthurG <cazooklol@gmail.com>
2025-03-19 15:37:55 -0300

    criando projeto spring para o backend e separando as pastas

---
+++ backendHairForce/src/main/java/com/br/HairForce/HairForceApplication.java
@@ -0,0 +1,13 @@
+package com.br.HairForce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairForceApplication.class, args);
+	}
+
+}
---
+++ backendHairForce/src/test/java/com/br/HairForce/HairForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.HairForce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
