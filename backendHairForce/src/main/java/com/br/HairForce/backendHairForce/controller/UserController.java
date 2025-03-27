eafcc09a43abe6f658490d4e63853fe66c2422a6
SaintArthurG <cazooklol@gmail.com>
2025-03-27 14:12:17 -0300

    adicionando schedule para acrescer ao dashboard no front

--- backendHairForce/src/main/java/com/br/HairForce/backendHairForce/controller/UserController.java
+++ backendHairForce/src/main/java/com/br/HairForce/backendHairForce/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/usuarios")
