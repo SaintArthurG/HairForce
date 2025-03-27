b9d738507f4cbce95903d911d9f7190f121af341
SaintArthurG <cazooklol@gmail.com>
2025-03-27 15:58:49 -0300

    adicionadno service de adicionar uma nova schedule, mas falta adicionar um Dto pra filtrar as informacoes

--- backendHairForce/src/main/java/com/br/HairForce/backendHairForce/service/ScheduleService.java
+++ backendHairForce/src/main/java/com/br/HairForce/backendHairForce/service/ScheduleService.java
@@ -5,6 +5,8 @@ import com.br.HairForce.backendHairForce.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ScheduleService {
 
@@ -15,7 +17,20 @@ public class ScheduleService {
         this.scheduleRepository = scheduleRepository;
     }
 
-    public Schedule saveSchedule(Schedule schedule)
+    public Schedule saveSchedule(Schedule schedule){
+        if(scheduleRepository.existByBarberIdAndHour(schedule.getBarber().getId(), schedule.getTime())){
+            throw new RuntimeException("Este horario ja esta reservado para este barbeiro");
+        }
+        return scheduleRepository.save(schedule);
+    }
+
+    public List<Schedule> scheduleList(){
+        return scheduleRepository.findAll();
+    }
+
+    public List<Schedule> listPerBarber(Long barberId){
+        return scheduleRepository.findByBarberId(barberId);
+    }
 
 
 
