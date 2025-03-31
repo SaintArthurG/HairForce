ce60920cd3a17cc5d3de6930507e1d6b94a641e6
SaintArthurG <cazooklol@gmail.com>
2025-03-31 08:27:07 -0300

    trocando o nome dashboard para schedule

--- backendHairForce/src/main/java/com/br/HairForce/backendHairForce/controller/HomeController.java
+++ backendHairForce/src/main/java/com/br/HairForce/backendHairForce/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 
-    @GetMapping    public String msg (){
+    @GetMapping public String msg (){
         return "oi";
     }
 }
