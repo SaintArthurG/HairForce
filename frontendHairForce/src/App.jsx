5a3781fd18e7f1c76c02820e70e650a1f2ef0481
Seidii <115049800+Seidii@users.noreply.github.com>
2025-04-21 19:29:10 -0300

    header

--- frontendHairForce/src/App.jsx
+++ frontendHairForce/src/App.jsx
@@ -1,24 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Registro/Signup";
 import './App.css'
 import SignupBarber from "./Components/Registro/SignupBarber";
-import Schedule from "./Components/Schedule/Schedule"
+import Schedule from "./Components/Schedule/Schedule";
 import ServiceRegistration from "./Components/Service/ServiceRegistration";
 
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="logo">HairForce</h1>
+      <nav className="nav">
+        <ul>
+          <li><Link to="/">Login</Link></li>
+          <li><Link to="/signup">Cadastro Cliente</Link></li>
+          <li><Link to="/signupbarber">Cadastro Barbeiro</Link></li>
+          <li><Link to="/schedules">Agendamentos</Link></li>
+          <li><Link to="/services">Serviços</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/signupbarber" element={<SignupBarber />} />
-                <Route path="/schedules" element={<Schedule />} />
-                <Route path="/services" element={<ServiceRegistration />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signupbarber" element={<SignupBarber />} />
+        <Route path="/schedules" element={<Schedule />} />
+        <Route path="/services" element={<ServiceRegistration />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
-
