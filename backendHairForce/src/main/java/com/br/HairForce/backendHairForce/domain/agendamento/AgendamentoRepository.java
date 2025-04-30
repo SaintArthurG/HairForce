422a420b879a488d8edb06a6f9eef79bbc79c6b9
SaintArthurG <cazooklol@gmail.com>
2025-04-29 22:43:57 -0300

    implementei o get dos agendamentos, falta o front puxar e arrumar os dados

--- backendHairForce/src/main/java/com/br/HairForce/backendHairForce/domain/agendamento/AgendamentoRepository.java
+++ backendHairForce/src/main/java/com/br/HairForce/backendHairForce/domain/agendamento/AgendamentoRepository.java
@@ -3,6 +3,9 @@ package com.br.HairForce.backendHairForce.domain.agendamento;
 import com.br.HairForce.backendHairForce.domain.barbeiro.Barbeiro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
     boolean existsByBarbeiroAndHora(Barbeiro barbeiro, String hora);
+    List<Agendamento> findAllByAtivoTrue();
 }
