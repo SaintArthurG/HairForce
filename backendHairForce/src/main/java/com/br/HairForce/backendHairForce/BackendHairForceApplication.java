6158e0979a96e8cb9d3e55bd4477813f06aa25b1
SaintArthurG <cazooklol@gmail.com>
2025-05-20 16:37:41 -0300

    docs: edit application.properties

--- backendHairForce/src/main/java/com/br/HairForce/backendHairForce/BackendHairForceApplication.java
+++ backendHairForce/src/main/java/com/br/HairForce/backendHairForce/BackendHairForceApplication.java
@@ -12,6 +12,7 @@ public class BackendHairForceApplication {
 
 		//DB
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_URL_TEST", dotenv.get("DB_URL_TEST"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
